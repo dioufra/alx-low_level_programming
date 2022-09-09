1d5dd22f13d1205d5bd1e55ce769f38575b1f065
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-09-09 11:55:05 -0700

    Check whether the number stored in the variable n is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,24 +15,20 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	/* n is greater than 0 */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	
 	/* n is equal to 0 */
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	
 	/* n is less than 0 */
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
-	
 	return (0);
 }
