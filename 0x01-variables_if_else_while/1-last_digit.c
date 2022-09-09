32e0c7869d9fdffac99cd01d1f8567e5f9d109c7
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-09-09 12:57:31 -0700

    Get the last digit of a number and compare it to 0, 5, and 6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
-	if (last_digit == 0)
+	else if (last_digit == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
-	if (last_digit < 0 && last_digit != 0)
+	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
