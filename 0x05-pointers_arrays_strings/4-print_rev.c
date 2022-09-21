e1d129937d84ae78f30ecf6b36c3af801becfe88
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-09-20 18:41:32 -0700

    Fix styling

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - function that prints a string, in reverse, 
+ * print_rev - function that prints a string, in reverse,
  * followed by a new line
  * @s: string to print
  */
