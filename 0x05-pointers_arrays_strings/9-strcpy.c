0f49ebe2389c146c98638a59e4fdb3be646e42a3
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-09-20 19:10:30 -0700

    Add function description

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _trcpy - Write a function that reverses a string.
+ * _strcpy - Write a function that reverses a string.
  * @dest: destination of copy
  * @src: source of copy
  * Return: destination(dest) pointer
