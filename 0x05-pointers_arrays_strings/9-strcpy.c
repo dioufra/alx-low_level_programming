0e38144af718cb90db8f9d97b43917ef36895bdc
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-09-20 18:16:59 -0700

    function that copies the string pointed to by src to the buffer pointed by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * trcpy - Write a function that reverses a string.
+ * @dest: destination of copy
+ * @src: source of copy
+ * Return: destination(dest) pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *tmp = dest;
+
+	while ((*scr))
+	{
+		*desc = *src;
+		scr++;
+		dest++;
+	}
+	return (tmp);
+}
