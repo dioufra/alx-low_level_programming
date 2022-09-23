2ffb755300c6f225eceb97fd7adb6499e45c3888
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-09-23 09:49:36 -0700

    Changed code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
- * _strcat - function that concatenate two strings
- * @src: source string
- * @dest: destination sring
- * Return:pointer to the concatenated string
+ * _strcat - concatenates two strings
+ * @dest: string to be modified.
+ * @src: string to be copied to the end of end.
+ * Return: pointer to dest.
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int len_dest = 0;
-	int len_src = 0;
+	int i = 0;
 
-	while (*dest++)
-		len_dest++;
-
-	while (*src++)
-		dest[len_dest + len_src] = src[len_src];
-
-	dest[len_dest + len_src] = '\0';
+	for (; *(dest + i) != '\0'; i++)
+	{
+	}
+	for (; *src != '\0'; src++)
+	{
+		*(dest + i) = *src;
+		i++;
+	}
+	*(dest + i) = *src;
 
 	return (dest);
+
 }
