7f9f926a1fbf813f411d8afec9b6210c52f51d0f
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-09-27 15:35:09 -0700

    Locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to search for
+ *
+ * Return: return pointer to the beginning of the located substring
+ * if no match found return NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j, x;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	while (haystack[i] != '\0') /* iterate through haystack */
+	{
+		/* if a byte matches first char of needle */
+		/* interate through needle until match ends */
+		if (haystack[i] == needle[0])
+		{
+			x = i, j = 0;
+			while (needle[j] != '\0')
+			{
+				if (haystack[x] == needle[j])
+					x++, j++;
+				else
+					break;
+			} /* if matched throughout, return haystack */
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
+		}
+		i++;
+	}
+	return (NULL); /* No match */
+}
