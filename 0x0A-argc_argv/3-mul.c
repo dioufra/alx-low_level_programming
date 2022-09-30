36991b4d9f58093c7252e7fa27fe61921e5f22f8
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-09-30 13:24:58 -0700

    Change code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,15 +13,12 @@ int main(int argc, char *argv[])
 	int i;
 	int result = 1;
 
-	if (argc > 1)
+	if (argc != 3)
 	{
-		for (i = 1; i < argc; i++)
-			result *= atoi(argv[i]);
-		printf("%d\n", result);
-
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
 
-	printf("Error\n");
-	return (1);
+	printf("%d\n", atoi(argv[1] * atoi(argv[2]);
+	return (0);
 }
