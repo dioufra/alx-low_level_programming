85018e1a6e28d82b0323c0d0a37fe22ca602ffd1
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-09-30 12:46:25 -0700

    Fix code description

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * maim - programm that print all arguments it receives.
+ * main - programm that print all arguments it receives.
  * @argc: number of arguments
  * @argv: pointer to the array of arguments
- * Retrun: Always 0 on success
+ * Return: Always 0 on success
  */
 
 int main(int argc, char *argv[])
