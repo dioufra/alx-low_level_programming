f1116944daefe9c17eea988704ed6042f1db2817
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-10-11 09:07:56 -0700

    Fix code

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,6 +2,7 @@
 
 /**
  * init_dog - initialize of variable of type struct dog
+ *
  * @d: pointer to struct dog to initialize
  * @name: name to initialize
  * @age: age to initialize
@@ -12,7 +13,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (!d)
 		return;
-
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
