a8f35ee277c15dd39e1d48bfda459b6ba8df6c6b
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-10-13 12:54:35 -0700

    Fix style

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * - int_index - function that searches for an integer.
+ * int_index - function that searches for an integer.
  *
  * @array: array of integers
  * @size: number of elements in the array
@@ -22,7 +22,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-				return (i);
+			return (i);
 	}
 
 	return (-1);
