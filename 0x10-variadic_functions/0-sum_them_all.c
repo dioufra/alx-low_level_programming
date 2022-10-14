45a4e1c39ce3568a4180901a748a87ebba0d9050
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-10-14 13:35:43 -0700

    Initial commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums the parameters
+ *
+ * @n: required parameter
+ *
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int sum = 0, i;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+
+	return (sum);
+}
