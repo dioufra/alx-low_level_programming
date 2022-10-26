7e0f02a19b16bf8a53fde48e774688790e288f13
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-10-26 16:59:33 -0700

    Fix code and style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new = malloc(sizeof(listint_t));
 
-	if (new == NULL || head = NULL)
+	if (new == NULL || head == NULL)
 		return (NULL);
 
 	new->n = n;
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (i = 0; tmp && i < idx; i++)
 	{
-		if (i = idx -1)
+		if (i == idx - 1)
 		{
 			new->next = tmp->next;
 			tmp->next = new;
