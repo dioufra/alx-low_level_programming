8b3b8bd6b57c3d2b4450847fa9ff8397e36e9f9c
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-10-28 12:52:34 -0700

    Gets the value of bit a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the value of the bit at a given index.
+ *
+ * @n: number
+ * @index: index
+ *
+ * Return: valueof the bit, or -1 if an error occur
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	max_bits_number = malloc(sizeof(unsigned long int) * 8);
+
+	if (index >= max_bits_number)
+		return (-1);
+
+	bit = ((n >> index) & 1);
+
+	return (bit);
+}
