01412b8358f814a5c8b40db8c62c6816e293e011
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-10-28 13:19:03 -0700

    clear bit to 0 at given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - gets the value of the bit at a given index.
+ *
+ * @n: number
+ * @index: index
+ *
+ * Return: 1 on success, -1 on error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int m = 1;
+	unsigned int max_bits_number;
+
+	max_bits_number = (sizeof(unsigned long int) * 8);
+
+	if (index >= max_bits_number)
+		return (-1);
+
+	m = ~(m << index);
+	*n = (*n & m);
+
+	return (1);
+}
