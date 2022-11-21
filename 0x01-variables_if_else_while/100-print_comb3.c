2ac79c81c06b67293c619d876bf5dde98c44484a
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-11-21 12:13:23 -0800

    print combination of numbers from 01 to 89

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print all combinations of two digits form 01 through 89
+ * Retturn: Always 0
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 48; n1 <= 56; n1++)
+	{
+		for (n2 = n1 + 1; n2 <= 57; n2++)
+		{
+			if (n1 != n2)
+			{
+				putchar(n1);
+				putchar(n2);
+				if (n1 != 56 || n2 != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
