6119971216a1a825397255641ef5fb44033d30bc
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-11-21 11:42:52 -0800

    Prints the sum of fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even-valued terms of the fibonacci numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long prevNum1 = 1, prevNum2 = 2, nextNum;
+	unsigned long sum = 2;
+	int counter = 0;
+
+	for (counter = 2; counter <= 49; counter++)
+	{
+		nextNum = prevNum1 + prevNum2;
+		if (nextNum % 2 == 0)
+			sum += nextNum;
+		prevNum1 = prevNum2;
+		prevNum2 = nextNum;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
