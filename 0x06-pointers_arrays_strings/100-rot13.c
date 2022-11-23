08de4183771bccca284f8d53fc5d1b372db6275d
François Pape Diouf <fanfandiouf2016@gmail.com>
2022-11-23 04:43:03 -0800

    encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
+ * Return: encoded string
+ */
+char *rot13(char *a)
+{
+	char alpha[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+			'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+			'w', 'x', 'y', 'z'};
+	char rot13[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 
+			'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+			'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
+			'X', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+			'j', 'k', 'l', 'm'};
+
+	int i, j;
+
+	for (i = 0; a[i]; i++)
+	{
+		for (j = 0; j <= 52; j++)
+		{
+			if (a[i] == alpha[j])
+			{
+				a[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (a);
+}
