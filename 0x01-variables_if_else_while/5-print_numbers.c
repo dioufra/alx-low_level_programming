0ae79568421cfc38de6d00c8aba7fbef21a795de
dioufra <fanfandiouf2016@gmail.com>
2023-02-17 23:31:06 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print digits
+ * Description: prints all the numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar(num);
+	return (0);
+}
+
