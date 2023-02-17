d0467511a55299236fec82dd317c1255448036db
dioufra <fanfandiouf2016@gmail.com>
2023-02-17 23:26:12 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ * Description: print the alphabet in lowercase expect the letter q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
