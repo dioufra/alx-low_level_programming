f773e2581d1beac74812213a8cfa84f31faaa0e5
dioufra <fanfandiouf2016@gmail.com>
2023-02-23 00:26:44 +0000

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even-valued terms of the first 30 fibonacci numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long prevNum1 = 1, prevNum2 = 2, nextNum;
+	unsigned long sum = 2;
+	int counter;
+
+	for (counter = 0; counter < 30; counter++)
+	{
+		nextNum = prevNum1 + prevNum2;
+		if (nextNum % 2 == 0)
+			sum += nextNum;
+		prevNum1 = prevNum2;
+		prevNum2 = nextNum;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
