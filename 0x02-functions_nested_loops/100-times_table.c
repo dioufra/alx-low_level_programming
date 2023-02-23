872b6d7c4dd0e6889a75572a9902d91b183c933b
dioufra <fanfandiouf2016@gmail.com>
2023-02-23 00:59:37 +0000

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table
+ * @n: table to print
+ */
+void print_times_table(int n)
+{
+	int i, j, res, nres;
+
+	if (n > 0 && n <= 15)
+	{
+		
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				res = i * j;
+				nres = i * (j + 1);
+
+				if (res <= 9)
+				{
+					_putchar(res + '0');
+				}
+				else if (res < 100)
+				{
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+				{
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10) % 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				if (j < n)
+				{
+					_putchar(',');
+					if (nres <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else if (nres < 100)
+					{ 
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else
+					{
+						_putchar(' ');
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
