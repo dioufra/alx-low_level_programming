a25b29e22e406017c6979cc953c5d5d19d3c6cb1
dioufra <fanfandiouf2016@gmail.com>
2023-02-28 23:32:49 +0000

    style: remove trailing whitespace

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,12 @@ void rev_string(char *str)
 	int i = 0;
 	int length = 0;
 	char tmp[512];
-	
+
 	while (*(str + length) != '\0')
 		length++;
-	
 	for (i = 0; i < length; i++)
 		*(tmp + i) = *(str + length - i - 1);
 	*(tmp + length) = '\0';
-	
 	for (i = 0; i < length; i++)
 		*(str + i) = *(tmp + i);
 	*(str + length) = '\0';
