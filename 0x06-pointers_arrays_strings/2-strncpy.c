596cd3de5b1da62b52c952a118b1b7ecc9a2fcd4
dioufra <fanfandiouf2016@gmail.com>
2023-03-02 21:18:48 +0000

    refactor: correct code requirements

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,27 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string
- * @dest: pointer to the destination string
- * @src: pointer to the source string
- * @n: number for byte to copy form src
+ * *_strncpy - function that copies a strings
  *
- * Return: A pointer to the destination string dest
+ * @src: source string
+ * @dest: destination sring
+ * @n: maximum number of bytes
+ *
+ * Return:pointer to the concatenated string
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (*(src + i) && i < n)
-		*(dest + i++) = *(src + i);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
 	while (i < n)
 	{
-		*(dest + i) = '\0';
+		dest[i] = '\0';
 		i++;
 	}
+
 	return (dest);
 }
