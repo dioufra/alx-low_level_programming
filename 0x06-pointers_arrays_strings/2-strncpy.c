48eac4540690b975015409525e005c3055cfc16a
dioufra <fanfandiouf2016@gmail.com>
2023-03-02 21:14:14 +0000

    style: fix styling

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,20 +6,20 @@
  * @src: pointer to the source string
  * @n: number for byte to copy form src
  * Description: copies at most n bytes of the the string pointed to by src,
- *              including the null terminated byte, to the buffer pointed to by dest.
- *              The string may not overlap, and the dest must be large enough
+ *		including the null terminated byte, to the buffer pointed to by dest.
+ *		The string may not overlap, and the dest must be large enough
  * Return: A pointer to the destination string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-        int i = 0;
+	int i = 0;
 
-        while (*(src + i) && i < n)
-                *(dest + i++) = *(src + i);
-        while (i < n)
+	while (*(src + i) && i < n)
+		*(dest + i++) = *(src + i);
+	while (i < n)
 	{
-                *(dest + i) = '\0';
+		*(dest + i) = '\0';
 		i++;
 	}
-        return (dest);
+	return (dest);
 }
