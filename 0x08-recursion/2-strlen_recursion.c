f8918c95eeada34d8c563c25df34991ea9c82335
dioufra <fanfandiouf2016@gmail.com>
2023-06-08 12:55:59 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the lenght of a string recursively
+ * @s: pointer the string
+ * Return: the lenght of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	return 1 + _strlen_recursion(s + 1);
+}
