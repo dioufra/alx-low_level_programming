2ec78713eb4f387b6f095691746907dcf75c7872
dioufra <fanfandiouf2016@gmail.com>
2023-06-08 12:37:00 +0000

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string followed by a newline recursivly
+ * @s: pointer to the address of the string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_puts_recursion(s + 1);
+	_putchar(*s);
+}
