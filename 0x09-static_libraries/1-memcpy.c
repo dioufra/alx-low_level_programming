6ad9d51d259f33caedd487b9ca71ae02964e5379
dioufra <fanfandiouf2016@gmail.com>
2023-06-09 20:42:55 +0000

    1-memcpy.c

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ *
+ * @src: memory area to copy from
+ * @dest: memory area to copy to
+ * @n: number of bytes to copy
+ *
+ * Return: a pointer to the memory area s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
