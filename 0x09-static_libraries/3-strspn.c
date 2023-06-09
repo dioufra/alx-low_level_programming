fcee9b4d50b92cdfc3c9f5dad5baee9ad5b59213
dioufra <fanfandiouf2016@gmail.com>
2023-06-09 22:27:26 +0000

    fix: betty style

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -5,24 +5,27 @@
  *
  * @s: string to search in
  * @accept: prefix substring
+ *
+ * Return: the number of bytes form s consisting of bytes from acceps
  */
 int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int i;
-    while (*s)
-    {
-        for (index = 0; accept[i]; i++)
-        {
-            if (*s == accept[i])
-            {
-                count++;
-                break;
-            }
-            else if (accept[i + 1] == '\0')
-                return (count);
-        }
-        s++;
-    }
-    return (count);
+	unsigned int count = 0;
+	int i;
+
+	while (*s)
+	{
+		for (index = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				count++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (count)
+		}
+		s++;
+	}
+	return (count);
 }
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -11,24 +11,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i;
+	int i;
 
-    if (*needle == 0)
-        return (haystack);
-    while (*haystack)
-    {
-        i = 0;
-
-        if (haystack[i] == needle[i])
-        {
-            while (haystack[i] == needle[i])
-            {
-                if (needle[i + 1] == '\0')
-                    return (haystack);
-                i++;
-            }
-        }
-        haystack++;
-    }
-    return ('\0');
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		i = 0;
+		if (haystack[i] == needle[i])
+		{
+			while (haystack[i] == needle[i])
+			{
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			}
+		}
+		haystack++;
+	}
+	return ('\0');
 }
