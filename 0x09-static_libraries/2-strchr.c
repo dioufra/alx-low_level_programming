cca49ee5d148ce257a4bf3c4855d869ca11489a0
dioufra <fanfandiouf2016@gmail.com>
2023-06-09 20:52:51 +0000

    2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ *
+ * @s: string to be searched
+ * @c: character to search
+ *
+ * Return: a pointer to the first occurence of the character c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*s)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
