fc9d06622fecd63e7296817408eef16ed48b7cd6
dioufra <fanfandiouf2016@gmail.com>
2023-06-10 08:13:37 +0000

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdlib.io>
+#include <stdio.h>
+
+/**
+ * main - prints all argument it receives
+ *
+ * @argc: number of command line arguments
+ * @argv: array containing the command line arguments
+ *
+ * Return: Always 0 (on success)
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("%c\n", "Error");
+		return (1);
+	}
+
+	return (atoi(argv[1]) * atoi(argv[2]));
+}
+
+
