aed2bff10a049f996fb50b8d15132575ffe806ac
dioufra <fanfandiouf2016@gmail.com>
2023-06-10 08:30:01 +0000

    fix: betty style

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,9 +12,9 @@
 
 int main(int argc, char **argv)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < argc; i++)
-        printf("%c\n", argv[i])
+	for (i = 0; i < argc; i++)
+		printf("%c\n", argv[i]);
 	return (0);
 }
