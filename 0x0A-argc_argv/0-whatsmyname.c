eaf9ec52c44506b3cff91dee52f372c174237c1e
dioufra <fanfandiouf2016@gmail.com>
2023-06-10 08:13:04 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdlib.io>
+#include <stdio.h>
+
+/**
+ * main - prints the name of a program followed by a newline
+ *
+ * @argc: number of command line arguments
+ * @argv: array containing the command line arguments
+ *
+ * Return: Always 0 (on success)
+ */
+
+int main(int __attribute__((__unused__)) argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
+
