f4e98378ec11a5339ca67b9ad837168c28c6ca12
frdiouf <fanfandiouf2016@gmail.com>
2023-06-20 20:12:44 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - struct
+ * @name: attribute
+ * @age: attribute
+ * @owner: attribute
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
