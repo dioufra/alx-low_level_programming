8750543a7ec025c16c093d60cafd5df99331bc94
frdiouf <fanfandiouf2016@gmail.com>
2023-06-22 12:27:32 +0000

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,9 +21,9 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	while (ops[i].operator != NULL)
+	while (ops[i].op != NULL)
 	{
-		if (*ops[i].operator == *s)
+		if (*ops[i].op == *s)
 			return (*ops[i].f);
 		i++;
 	}
