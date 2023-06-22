89a172456e88439594f9cfeaab76a66d0c520b18
frdiouf <fanfandiouf2016@gmail.com>
2023-06-22 18:00:45 +0000

    fix: and iteration and initialize sum to zero

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  * sum_them_all - function tha sum all it parameters
@@ -12,7 +13,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	int sum, i;
+	int sum = 0, i;
 
 	if (n == 0)
 		return (0);
@@ -20,7 +21,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_list(ap, int);
+		sum += va_arg(ap, unsigned int);
 
+	va_end(ap);
 	return (sum);
 }
