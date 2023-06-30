081c891cf18bca7d204cf07a7d897f1f784e9ce8
frdiouf <fanfandiouf2016@gmail.com>
2023-06-30 16:07:33 +0000

    Add header file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_list - prints all elements of a list
+ *
+ * @h: pointer to the first head of the list;
+ *
+ * Return: the number of nodes.
+ *
+ * if str is NULL print [0] (nil)
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
