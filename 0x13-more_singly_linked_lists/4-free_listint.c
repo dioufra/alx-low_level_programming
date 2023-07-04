6fc9023691410fe1610e1e1d92a629f82c1be311
frdiouf <fanfandiouf2016@gmail.com>
2023-07-04 20:31:12 +0000

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t
+ *
+ * @head: pointer to the first node of the list
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->n);
+		free(head);
+		head = head->next;
+	}
+}
